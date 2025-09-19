e57b0e8da2f14dd474a2a63f784fb0153fa5f186
leontoys <svliyon@gmail.com>
2025-09-20 06:43:57 +1000

    created add to do mutation

--- convex/todos.ts
+++ convex/todos.ts
@@ -1,4 +1,5 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
 export const getTodos = query({//function for reading data
     args: {},//no arguments 
@@ -7,5 +8,16 @@ export const getTodos = query({//function for reading data
             .order("desc")//order by creation time
             .collect()//return all matching todos as an array
         return todos
+    }
+})
+
+export const addTodo = mutation({
+    args: { text: v.string() },//takes tdotext
+    handler: async (ctx,args) => {
+        const todoId = await ctx.db.insert("todos", {//create a todo
+            text: args.text, 
+            isCompleted : false
+        })
+        return todoId
     }
 })
