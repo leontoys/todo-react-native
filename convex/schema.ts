ff0d6b724cceb30f48e2999dbf5c59d9aa0a5fe0
leontoys <svliyon@gmail.com>
2025-09-20 06:35:45 +1000

    created convex schema and table

---
+++ convex/schema.ts
@@ -0,0 +1,9 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({ //wraps entire db structure
+    todos: defineTable({//creates table
+        text: v.string(), //v - validators
+        isCompleted : v.boolean()
+    })
+})
