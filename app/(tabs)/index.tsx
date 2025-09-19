6b82c796e830b1cacc4d23c026e6b97a1d1fcfe2
leontoys <svliyon@gmail.com>
2025-09-20 08:28:52 +1000

    created home styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,43 +1,20 @@
+import { createHomeStyles } from '@/assets/settingsStyles';
 import useTheme from '@/hooks/useTheme';
 import { useMutation, useQuery } from "convex/react";
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { api } from "../../convex/_generated/api";
-
-const createStyles = (colors) => StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.background,
-    alignItems: 'center',
-    justifyContent : 'center'
-  },
-  content: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color : colors.text
-  }
-})
 
 export default function HomeScreen() {
   const todos = useQuery(api.todos.getTodos)
   console.log("Fetched todos", todos)
   const addTodo = useMutation(api.todos.addTodo)
   const { toggleDarkMode, colors } = useTheme()
-  const styles = createStyles(colors)
+  const styles = createHomeStyles(colors)
   
   return (
     <View style={styles.container}>
-      <Text style={styles.content}>Home</Text>
+      <Text>Home</Text>
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: 'red',
-//     flexDirection: 'row', // <-- Add this line
-//     gap: 10, // <-- Optional: Adds space between children
-//   },
-//   content: {
-//     fontSize: 52,
-//   },
-// });
