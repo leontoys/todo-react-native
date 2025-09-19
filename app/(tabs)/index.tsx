446b196c664238b8ec0797db6bd35c58df9372f8
leontoys <svliyon@gmail.com>
2025-09-20 08:21:08 +1000

    using create styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,43 @@
 import useTheme from '@/hooks/useTheme';
 import { useMutation, useQuery } from "convex/react";
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { api } from "../../convex/_generated/api";
+
+const createStyles = (colors) => StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    alignItems: 'center',
+    justifyContent : 'center'
+  },
+  content: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color : colors.text
+  }
+})
 
 export default function HomeScreen() {
   const todos = useQuery(api.todos.getTodos)
   console.log("Fetched todos", todos)
   const addTodo = useMutation(api.todos.addTodo)
-  const { toggleDarkMode } = useTheme()
+  const { toggleDarkMode, colors } = useTheme()
+  const styles = createStyles(colors)
   
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>Toggle </Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => addTodo({ text: "go for a walk" })} style={{
-        padding: 20,
-        backgroundColor: "#ccc",
-        marginTop : 20
-      }}>
-
-      </TouchableOpacity>
+      <Text style={styles.content}>Home</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     backgroundColor: 'red',
+//     flexDirection: 'row', // <-- Add this line
+//     gap: 10, // <-- Optional: Adds space between children
+//   },
+//   content: {
+//     fontSize: 52,
+//   },
+// });
